fa0616a1b5690dec90ae154baedbfcd048bc73f2
algarim <alvaro.garimberti@gmail.com>
2023-09-08 17:42:17 -0300

    1º version completed: services and team done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// HAMBURGER MENU
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -9,4 +11,21 @@ hamburger.addEventListener( "click", () => {
 document.querySelectorAll(".nav-link").forEach( link => link.addEventListener( "click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-} ) )
+} ) );
+
+
+// SERVICES TABS
+
+const serviceItems = document.querySelectorAll(".services-item");
+
+serviceItems.forEach( service => {
+    const icon = service.querySelector("i");
+    const description = service.querySelector(".service-description")
+
+    icon.addEventListener( "click", () => {
+        icon.classList.toggle("fa-plus");
+        icon.classList.toggle("fa-minus");
+
+        description.classList.toggle("active");
+    } )
+} )
